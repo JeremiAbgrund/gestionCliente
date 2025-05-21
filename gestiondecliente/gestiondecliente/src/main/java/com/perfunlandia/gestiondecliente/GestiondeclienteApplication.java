ad505a83d31383393a827c0a710f0ace1e87920d
jeremi <jeremiriquelme@gmail.com>
2025-05-20 21:00:42 -0400

    Aniadir a repositorio

---
+++ gestiondecliente/gestiondecliente/src/main/java/com/perfunlandia/gestiondecliente/GestiondeclienteApplication.java
@@ -0,0 +1,14 @@
+package com.perfunlandia.gestiondecliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement // Habilita la gestión de transacciones
+public class GestiondeclienteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestiondeclienteApplication.class, args);
+    }
+}
