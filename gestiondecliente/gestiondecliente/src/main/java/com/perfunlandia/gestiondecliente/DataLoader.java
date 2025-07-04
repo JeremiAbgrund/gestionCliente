ca1abe1b00b3f29acc5dc06574bde7162f5624cf
jeremi <jeremiriquelme@gmail.com>
2025-07-04 02:57:22 -0400

    Final

--- gestiondecliente/gestiondecliente/src/main/java/com/perfunlandia/gestiondecliente/DataLoader.java
+++ gestiondecliente/gestiondecliente/src/main/java/com/perfunlandia/gestiondecliente/DataLoader.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.perfunlandia.gestiondecliente.model.Cliente;
@@ -11,7 +12,7 @@ import com.perfunlandia.gestiondecliente.repository.ClienteRepository;
 
 import net.datafaker.Faker;
 @SuppressWarnings("deprecation")
-//@Profile("dev")
+@Profile("dev")
 @Component
 public class DataLoader implements CommandLineRunner {
 
--- gestiondecliente/gestiondecliente/src/test/java/com/controller/ClienteControllerTest.java
+++ gestiondecliente/gestiondecliente/src/test/java/com/controller/ClienteControllerTest.java
@@ -21,6 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 import java.util.Optional;
 
+
 @WebMvcTest(GestionClienteController.class)
 @ContextConfiguration(classes = GestiondeclienteApplication.class)
 public class ClienteControllerTest {
