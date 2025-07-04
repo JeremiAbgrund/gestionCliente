548ad334750d979e456b9d5a2ca182dcf053b1c3
jeremi <jeremiriquelme@gmail.com>
2025-07-04 03:29:38 -0400

    Final

--- gestiondecliente/gestiondecliente/src/main/java/com/perfunlandia/gestiondecliente/GestiondeclienteApplication.java
+++ gestiondecliente/gestiondecliente/src/main/java/com/perfunlandia/gestiondecliente/GestiondeclienteApplication.java
@@ -2,10 +2,12 @@ package com.perfunlandia.gestiondecliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement // Habilita la gestión de transacciones
+@EnableJpaRepositories("com.perfunlandia.gestiondecliente.repository")
 public class GestiondeclienteApplication {
 
     public static void main(String[] args) {
